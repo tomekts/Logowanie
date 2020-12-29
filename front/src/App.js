9847229e99ec4184f0503022fb4647c7253e9fa0
Tomasz <tomek_kasperczyk@interia.pl>
2020-12-29 15:08:22 +0100

    dodanie reacta

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Component } from 'react';
+
+import Rejestracja from './Rejestracja.js'
+
+class App extends Component {
+  render(){
+    return <Rejestracja />
+    
+  }
+}
+
+export default App;
---
+++ front/src/Rejestracja.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { Component } from 'react';
+
+class Rejestracja extends Component {
+  render(){
+    return "rejestracja222"
+  }
+}
+
+export default Rejestracja;
